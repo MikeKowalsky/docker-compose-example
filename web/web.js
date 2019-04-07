6b93debca3c03c470831512592c6a98e1c2036ee
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-07 12:39:45 +0200

    added web server

---
+++ web/web.js
@@ -0,0 +1,27 @@
+const { join } = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/newsletter", async (req, res) => {
+  const response = await axios.post("", {
+    email: req.body.email
+  });
+
+  res.json(response.data);
+});
+
+app.listen(process.env.PORT || 80, err => {
+  if (err) throw err;
+  console.log("Server is up.");
+});

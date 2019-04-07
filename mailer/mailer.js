684e462f4e4bbe6e84421aa867509f10309f835a
MikeKowalsky <mkowalewski83@gmail.com>
2019-04-07 12:41:28 +0200

    added mailer server

---
+++ mailer/mailer.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post("/send-mail", (req, res) => {
+  //send email logic
+  res.json({ message: `E-mail has been send to ${req.body.email}` });
+});
+
+app.listen(process.env.PORT || 80, err => {
+  if (err) throw err;
+  console.log("Server is up.");
+});
